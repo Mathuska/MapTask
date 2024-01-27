39b06a7d74926dd30af7812729d0a2ff23c1158e
Cosarca Maxim <cosarcamaxim@gmail.com>
2024-01-27 02:19:27 +0200

    Add map

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -1,20 +1,35 @@
-import React, { useRef, useEffect } from "react";
+import { useRef, useState , useEffect } from "react";
+import { APIProvider } from "@vis.gl/react-google-maps";
+import { Marker } from '../interfaces/mapinterfaces';
 
 const MapPage = () => {
-  const harta = useRef(null);
-
+  const initialPosition = { lat: 47.0122, lng: 28.8605 };
+  const harta = useRef<HTMLDivElement>(null)
+  const [markers, setMarkers] = useState<Marker[]>([]);
+  
+      
   useEffect(() => {
     if (harta.current) {
-      const map = new window.google.maps.Map(harta.current, {
-        center: { lat: 47.0122, lng: 28.8605 }, 
-        zoom: 8, 
+      const map = new google.maps.Map(harta.current, {
+        zoom: 12,
+        center: initialPosition,
+      });
+      map.addListener("click", (event : any) => {
+        handleMapClick(event);
       });
     }
-  }, [harta]);
+  }, []);
+
+  const handleMapClick = (event: any) => {
+    const position = {lat : event.latLng.lat() , lng :event.latLng.lng() }
+        
+        console.log(position);
+  };
+
   return (
-  <div ref={harta} style={{ width: "100%", height: "100vh" }}>
-    
-  </div>
+    <APIProvider apiKey={"AIzaSyDRsvFP1wiFpD2tkrz_a2XGNR1bR79PR3s"}>
+      <div style={{ height: "100vh", width: "100%" }} ref={harta}></div>
+    </APIProvider>
   );
 };
 
